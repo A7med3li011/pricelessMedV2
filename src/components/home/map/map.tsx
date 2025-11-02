ef990d3aef02886ea9ca0c73b74a34a48dce98cf
Ahmed Ali <a7med3li0111@gmail.com>
2025-11-02 20:00:38 +0200

    handle rerender map

--- src/components/home/map/map.tsx
+++ src/components/home/map/map.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import GoogleMapsProvider from "@/src/components/providers/GoogleMapsProvider";
+import { useEffect, useState } from "react";
 
 interface MapProps {
   long: number;
@@ -18,8 +19,15 @@ const MapContent = dynamic(() => import("./MapContent"), {
 });
 
 export default function Map({ long, lat }: MapProps) {
+  const [mapKey, setMapKey] = useState(0);
+
+  // Force remount when component mounts (navigation to /)
+  useEffect(() => {
+    setMapKey((prev) => prev + 1);
+  }, []);
+
   return (
-    <GoogleMapsProvider>
+    <GoogleMapsProvider key={mapKey}>
       <MapContent lat={lat} long={long} />
     </GoogleMapsProvider>
   );
